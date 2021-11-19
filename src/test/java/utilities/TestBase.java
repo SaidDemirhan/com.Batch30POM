3ef2c55dc6c5a6c9ba5d43ebd700a1c962f10ecf
said <saiddemirhan@gmail.com>
2021-11-19 12:28:22 +0100

    @BeforeClass

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -3,31 +3,30 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
-
-
+public abstract class TestBase {
     // abstract yaparak bu class'dan obje olusturulmasinin onune geceriz
     protected WebDriver driver;
     // biz test base class'i sadece extends ile inherit ederek kullanacagiz
-    // dolayisiyla olusturdugumuz driver variable'i icin protected access
-    // modifier'i seciyoruz
+    // dolayisiyla olusturdugumuz driver variable'i icin protected access modifier'i seciyoruz
 
 
-    @BeforeMethod
+    @BeforeClass
     public void setup(){
         WebDriverManager.chromedriver().setup();
-        driver= new ChromeDriver();
+        driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
-    @AfterMethod
+
+    @AfterClass
     public void tearDown(){
+
         driver.quit();
     }
 }
